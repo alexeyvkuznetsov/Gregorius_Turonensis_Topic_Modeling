04c3ed5f5c7b55053baa9dadc887fef7dee4bbef
alexeyvkuznetsov <57808789+alexeyvkuznetsov@users.noreply.github.com>
2020-12-14 00:43:18 +0300

    Update text2vec.R

--- text2vec.R
+++ text2vec.R
@@ -31,5 +31,34 @@ lsa = LSA$new(n_topics = 10)
 doc_embeddings =  fit_transform(dtm, tfidf)
 doc_embeddings =  fit_transform(doc_embeddings, lsa)
 
+tokens = tolower(movie_review$review[1:4000])
+tokens = word_tokenizer(tokens)
+it = itoken(tokens, ids = movie_review$id[1:4000], progressbar = FALSE)
+v = create_vocabulary(it)
+v = prune_vocabulary(v, term_count_min = 10, doc_proportion_max = 0.2)
+
+vectorizer = vocab_vectorizer(v)
+dtm = create_dtm(it, vectorizer, type = "dgTMatrix")
+
+lda_model = LDA$new(n_topics = 10, doc_topic_prior = 0.1, topic_word_prior = 0.01)
+doc_topic_distr = 
+  lda_model$fit_transform(x = dtm, n_iter = 1000, 
+                          convergence_tol = 0.001, n_check_convergence = 25, 
+                          progressbar = FALSE)
+
+
+barplot(doc_topic_distr[1, ], xlab = "topic", 
+        ylab = "proportion", ylim = c(0, 1), 
+        names.arg = 1:ncol(doc_topic_distr))
+
+
+lda_model$get_top_words(n = 10, topic_number = c(1L, 5L, 10L), lambda = 1)
+
+lda_model$get_top_words(n = 10, topic_number = c(1L, 5L, 10L), lambda = 0.2)
+
+
+perplexity(new_dtm, topic_word_distribution = lda_model$topic_word_distribution, doc_topic_distribution = new_doc_topic_distr)
+
+lda_model$plot()
 
 
